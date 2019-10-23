48f43a1a7eea24e6c3969939533300039bb1ec6b
Alexandre Campinho <alexandrearc@gmail.com>
2019-10-23 22:28:34 +1300

    Add JToken request api binding example.

---
+++ Core.Api.Playground/Controllers/BeachesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Core.Api.Playground.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BeachesController : ControllerBase
+    {
+        /// <summary>
+        /// We want to test that the incoming request can bind with request class
+        /// Just for fun we will return the object in XML
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>XML Beaches</returns>
+        [HttpPost]
+        public IActionResult Post([FromBody] BeachRequest request)
+        {
+            //We want to test that the incoming request can bind with request class
+            //Just for fun we will return the object in XML
+
+            //XML needs a root object
+            var root = new JObject();
+            root.Add("Beach", request.Beach);
+            var xmlBeaches = JsonConvert.DeserializeXmlNode(root.ToString());
+
+            return Ok(xmlBeaches);
+        }
+
+
+        public class BeachRequest
+        {
+            public JToken Beach { get; set; }
+        }
+    }
+}
